e7daa393c814c9c8c57816057192fc6a74a86c38
resolvewang <1796246076@qq.com>
2017-04-15 13:13:57 +0800

    自动化方式完成

--- baidu/auto_login.py
+++ baidu/auto_login.py
@@ -1,8 +1,5 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 def login(name, passwd):
